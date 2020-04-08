2d2047aacadbea4332ab1210148d280e9f41f1ed
Dare2python <31766642+Dare2python@users.noreply.github.com>
2020-04-08 18:41:46 +0300

    initial commit

---
+++ Question1/app.js
@@ -0,0 +1,14 @@
+(function retrieveMostFrequentlyUsedWords(literatureText, wordsToExclude){
+    console.log(literatureText);
+    console.log(wordsToExclude);
+    
+}("Jack and Jill went to the market to buy bread and cheese. Cheese is Jack's and Jill's favourite food.",
+ ["and",
+  "he",
+  "the",
+  "to",
+  "is",
+  "Jack",
+  "Jill"
+ ])
+)
