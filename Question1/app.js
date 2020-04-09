c5a547ba1286d6ea8719b0b5017cf41c2ce2a65c
Dare2python <31766642+Dare2python@users.noreply.github.com>
2020-04-09 17:18:04 +0300

    solution with debug
    first round of solution with staightforward approach

--- Question1/app.js
+++ Question1/app.js
@@ -1,8 +1,55 @@
-(function retrieveMostFrequentlyUsedWords(literatureText, wordsToExclude){
+
+
+(function retrieveMostFrequentlyUsedWords(literatureText, wordsToExclude) {
+    "use strict";
     console.log(literatureText);
-    console.log(wordsToExclude);
     
-}("Jack and Jill went to the market to buy bread and cheese. Cheese is Jack's and Jill's favourite food.",
+    let r = /[^A-Za-z]/;
+    let list = literatureText.split(r);
+    list = list.map( word => word.toLowerCase() );
+    console.log(list);
+    
+    let raw = list.reduce( 
+        (previous, curent, index) => {
+            previous.get(curent) ? 
+                previous.set(curent, previous.get(curent)+1):
+                previous.set(curent, 1);
+            return previous;
+        },
+        new Map()
+    );
+    console.log(raw);
+    
+    let exclude = new Set(wordsToExclude.map( word => word.toLowerCase() ));
+    exclude.add("");
+    console.log(exclude);
+    
+    let result = new Array();
+    let clean = new Map();
+    let maxValue = 0;
+
+    raw.forEach( (value, key) => {
+        if( !exclude.has(key) ){
+//            console.log(key, value);
+            maxValue = value>maxValue?value:maxValue;
+            clean.set(key, value);
+        }
+    }); 
+    console.log(maxValue);
+    console.log(clean);
+    
+    clean.forEach( (value, key) => {
+        if( value === maxValue ){
+            result.push(key);
+        }
+    }); 
+    
+    console.log(result);
+    
+    return result;
+    
+    
+} ("Jack and Jill went to the market to buy bread and cheese. Cheese is Jack's and Jill's favourite food.",
  ["and",
   "he",
   "the",
